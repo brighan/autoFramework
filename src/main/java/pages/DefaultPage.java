14d280745414471269ab4fe6dac8154abea339e4
Brighan Santamaria <brighan10@gmail.com>
2020-08-10 15:08:27 -0500

    Add PageFactory to Consructor

--- src/main/java/pages/DefaultPage.java
+++ src/main/java/pages/DefaultPage.java
@@ -3,10 +3,12 @@ package pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class DefaultPage extends BasePageSettings {
     public DefaultPage(WebDriver driver) {
         super(driver);
+        PageFactory.initElements(driver,this);
     }
 
     @FindBy(id = "ID_OF_ELEMENT")
